4893bd1ab824fa19e90638b87b5fb09940d5aed1
Kim Anh Nguyen <amazarashi2002@gmail.com>
2024-06-18 23:03:28 +0700

    Update Firebase setup, save form data

---
+++ app/(tabs)/ProfileScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ProfileSreen() {
+    return (
+        <View>
+            <Text>Homesssdsdscreen</Text>
+        </View>
+    )
+}
---
+++ app/LoginScreen.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import {View,StyleSheet, Text, TouchableOpacity} from 'react-native';
+import * as WebBrowser from 'expo-web-browser';
+import { useOAuth } from "@clerk/clerk-expo";
+import {useWarmUpBrowser} from "@/hooks/useWarmUpBrowser";
+ WebBrowser.maybeCompleteAuthSession();
+
+export default function LoginScreen() {
+    useWarmUpBrowser();
+
+    const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+    const onPress = React.useCallback(async () => {
+        try {
+            const { createdSessionId, signIn, signUp, setActive } =
+                await startOAuthFlow();
+
+            if (createdSessionId) {
+                setActive?.({ session: createdSessionId });
+            } else {
+                // Use signIn or signUp for next steps such as MFA
+            }
+        } catch (err) {
+            console.error("OAuth error", err);
+        }
+    }, [startOAuthFlow]);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.content}>
+                <Text style={styles.title}>Planto!</Text>
+                <Text style={styles.subtitle}>Buy - Sell Your Babies</Text>
+                <TouchableOpacity style={styles.button} onPress={onPress}>
+                    <Text style={styles.buttonText}>Get started</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f5',
+    },
+    content: {
+        alignItems: 'center',
+        padding: 20,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 8,
+        elevation: 5,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    subtitle: {
+        fontSize: 16,
+        color: '#555',
+        marginBottom: 20,
+    },
+    button: {
+        backgroundColor: '#007bff',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+    },
+})
